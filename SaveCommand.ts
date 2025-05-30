12e890b7d740bf9873ebd86b746917eb2ed06d45
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2025-05-30 15:36:09 -0400

    creacion de save

---
+++ SaveCommand.ts
@@ -0,0 +1,18 @@
+// SaveCommand.ts
+import Command from './Command';
+import ItemListManager from './ItemListManager'; 
+
+class SaveCommand implements Command {
+    private itemListManager: ItemListManager;
+
+    constructor() {
+        this.itemListManager = ItemListManager.getInstance();
+    }
+
+    execute(): void {
+      
+        console.log('Datos guardados (o se intentó guardar).');
+        
+    }
+}
+export default SaveCommand;
